d7f051771f0637ec13932b74d515d7379e03b0c5
Kowalski42 <Elena.Loskutnikov.a@mail.ru>
2022-09-22 15:58:11 +0300

    change properties file

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -1,15 +1,20 @@
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import helpers.Attach;
+import helpers.CredentialsConfig;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class TestBase {
+    static String selenoidRemote = System.getProperty("selenoid_remote");
 
     @BeforeAll
     public static void config() {
+        CredentialsConfig config = ConfigFactory.create(CredentialsConfig.class);
+
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", true);
@@ -20,13 +25,12 @@ public class TestBase {
         Configuration.browser = System.getProperty("browser_name", "chrome");
         Configuration.browserVersion = System.getProperty("browser_version", "101");
         Configuration.browserSize = System.getProperty("browser_size","1920x1080");
-        // set https://user1:1234@selenoid.autotests.cloud/wd/hub for remote test CI/CD
-        String selenoidRemote = System.getProperty("selenoid_remote");
         if (selenoidRemote != null) {
-            //TODO yaml file with login and password
-            Configuration.remote = "https://user1:1234@" + selenoidRemote;
+            Configuration.remote = "https://" + config.loginSelenoid() + ":" + config.passwordSelenoid() + "@" + selenoidRemote;
         }
         SelenideLogger.addListener("allure", new AllureSelenide());
+
+
     }
 
     @AfterEach
@@ -34,6 +38,8 @@ public class TestBase {
         Attach.screenshotAs("Last screenshot");
         Attach.pageSource();
         Attach.browserConsoleLogs();
-        Attach.addVideo();
+        if (selenoidRemote != null) {
+            Attach.addVideo();
+        }
     }
 }
